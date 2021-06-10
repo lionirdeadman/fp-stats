441951a5fc3388040c12665ee3f0750987ba79c4
Lionir <git@thelion.website>
2021-06-09 23:20:46 -0400

    Add submodule handling

--- flathub-application-cloner.py
+++ flathub-application-cloner.py
@@ -85,6 +85,9 @@ for i in range(18):
             print(repo_name + " is already cloned, pulling...")
             command = f"( cd flathub/{repo_name}; git pull )"
             os.system(command)
+            if os.path.isdir(f"flathub/{repo_name}/.gitmodules"):
+                command2 = f"(cd flathub/{repo_name}; git submodule update --init --recursive)"
+                os.system(command2)
         else:
             command = f"git clone --recursive {repo_url} flathub/{repo_name}"
             os.system(command)
